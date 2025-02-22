75097987f53db468df6fe172b235b8298a491279
diyaprajapati <diyabprajapati2005@gmail.com>
2025-02-22 18:47:15 +0530

    added account route

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,7 +1,14 @@
 import { Separator } from "@/components/ui/separator";
 import { AccountForm } from "./account/account-form";
+import { useSession } from "next-auth/react";
+import React from "react";
 
 export default function SettingsAccountPage() {
+
+    const [loading, setLoading] = React.useState(false);
+
+    const { data: session, status } = useSession();
+
     return (
         <div className="space-y-6">
             <div>
@@ -12,7 +19,7 @@ export default function SettingsAccountPage() {
                 </p>
             </div>
             <Separator />
-            <AccountForm />
+            <AccountForm user={{ email: session?.user.email!, name: session?.user.name! }} />
         </div>
     )
 }
