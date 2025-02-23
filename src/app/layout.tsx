78adc0392f56040ba3b28f910e59f758d9d5e484
diyaprajapati <diyabprajapati2005@gmail.com>
2025-02-23 17:59:18 +0530

    correct the name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "mANndmap",
+  title: "mAIndmap",
   description: "AI Generate mindmaps for your studies",
 };
 
